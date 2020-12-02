64c38d0f05b159e6a7bb81317ac9261379f587ef
Tom Hemery <tomhemery@outlook.com>
2020-12-02 12:34:39 +0000

    Removed unused using System from Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-
-namespace AdventOfCode2020
+﻿namespace AdventOfCode2020
 {
     class Program
     {
         static void Main(string[] args)
         {
             PasswordValidator.ValidatePartTwo("input/pwds.txt");
-
         }
     }
 }
